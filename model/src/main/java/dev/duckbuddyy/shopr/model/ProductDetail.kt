af4f1771a6c9d95ef62128454de061d5511601c0
Emirhan Soylu <emirhansoylu98@gmail.com>
2023-09-26 13:05:45 +0300

    ProductDetail entity and model created

---
+++ model/src/main/java/dev/duckbuddyy/shopr/model/ProductDetail.kt
@@ -0,0 +1,26 @@
+package dev.duckbuddyy.shopr.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ProductDetail(
+    @SerialName("description")
+    val description: String,
+    @SerialName("image")
+    val image: String,
+    @SerialName("name")
+    val name: String,
+    @SerialName("price")
+    val price: Int,
+    @SerialName("product_id")
+    val productId: String
+) {
+    fun toProductDetailEntity() = ProductDetailEntity(
+        image = image,
+        name = name,
+        price = price,
+        productId = productId,
+        description = description
+    )
+}
---
+++ model/src/main/java/dev/duckbuddyy/shopr/model/ProductDetailEntity.kt
@@ -0,0 +1,22 @@
+package dev.duckbuddyy.shopr.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "product_detail")
+data class ProductDetailEntity(
+    val description: String,
+    val image: String,
+    val name: String,
+    val price: Int,
+    @PrimaryKey
+    val productId: String
+) {
+    fun toProductDetail() = ProductDetail(
+        image = image,
+        name = name,
+        price = price,
+        productId = productId,
+        description = description
+    )
+}
